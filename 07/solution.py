6ae8cc983c7ddfa88e1c975c57ba37e08ff56664
Chris Deigan <chris@deigan.id.au>
2021-12-07 10:03:02 +0000

    Day 7

---
+++ 07/solution.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+
+def part1(crabs):
+    return solve(crabs)
+
+
+def part2(crabs):
+    return solve(crabs, part2=True)
+
+
+def solve(crabs, part2=False):
+    minpos = None
+    mincost = None
+    for pos in range(min(crabs), max(crabs) + 1):
+        cost = sum(
+            map(
+                lambda crab: abs(pos - crab)
+                if not part2
+                else int(abs(pos - crab) * ((abs(pos - crab) + 1) / 2)),
+                crabs,
+            )
+        )
+        if not mincost or cost < mincost:
+            minpos = pos
+            mincost = cost
+    return mincost
+
+
+def test():
+    sampleinput = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+    assert part1(sampleinput) == 37
+    assert part2(sampleinput) == 168
+
+
+if __name__ == "__main__":
+    input = list(map(int, open("input", "r").read().strip().split(",")))
+    print("Part 1: {}".format(part1(input)))
+    print("Part 2: {}".format(part2(input)))
